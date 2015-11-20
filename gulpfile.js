37527efa98ac4a2a13e829fc43003924a0711626
Joel Kreutzwieser <kreutzwj@gmail.com>
2015-11-20 12:31:34 +0900

    Started development of item

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@ var gulp = require('gulp');
 gulp.task('default', function () {
 });
 
+gulp.task("watch_dev", function () {
+    gulp.watch(["./**/*.js", "./**/*.html"], ["test:local"]).on("change", function (file) {
+        console.log("Files changed");
+    });
+});
 
 gulp.task('ci', ['test:remote']);
 
